9692716f8b780d730cf144c8a7b297a53f384791
Álvaro <ainiguezdislaa@gmail.com>
2023-02-25 13:56:00 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej14_1;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        int [] lista1={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(dominio.ej14_1.listacuadrados(lista1));
+    }
 }
--- src/dominio/ej14_1.java
+++ src/dominio/ej14_1.java
@@ -1,2 +1,13 @@
-package dominio;public class ej14_1 {
+package dominio;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class ej14_1 {
+    public static ArrayList listacuadrados(int[] lista1){
+
+        return Stream.iterate(0,i->i+1).limit(lista1.length).map(i->lista1[i]*lista1[i]).collect(Collectors.toCollection(ArrayList::new));
+
+    }
 }
